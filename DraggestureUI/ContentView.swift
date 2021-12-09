5aef456a9b4895589c8e79cd9792522a44a46f9d
Anbya Adzka <68680955+anbya26@users.noreply.github.com>
2021-12-09 13:42:45 +0700

    Add files via upload

---
+++ DraggestureUI/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  DraggestureUI
+//
+//  Created by Mufti Ramdhani on 16/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var showMenu = false
+  @State private var offset = CGSize.zero
+    var body: some View {
+      
+      let drag  = DragGesture()
+        .onChanged{
+          gesture in
+          self.offset = gesture.translation
+        }
+        .onEnded {
+          value in
+          self.offset = CGSize.zero
+          self.showMenu = false
+        }
+      
+      return ZStack{
+        Color.orange
+          .edgesIgnoringSafeArea(.all)
+        VStack{
+          Image("dirgahayu")
+            .resizable()
+            .frame(width:300, height:300)
+          Button(action: {
+            withAnimation {
+              self.showMenu.toggle()
+            }
+          }){
+            Text("Promo Merdeka").padding().background(Color.green).foregroundColor(Color.white).cornerRadius(20)
+          }
+          
+          
+           
+        }
+        
+        if self.showMenu {
+          Kupon()
+            .transition(.move(edge: .bottom)).animation(.default)
+            .offset(y:self.offset.height)
+            .gesture(drag)
+            
+        }
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Kupon : View {
+  var body: some View {
+    VStack{
+      Text("Selamat anda mendapatkan kupon merdeka sebanyak 500 point").font(.largeTitle).bold().foregroundColor(Color.white)
+    }
+    .frame(minWidth:0, maxWidth:.infinity, minHeight: 0, maxHeight:.infinity)
+    .background(Color.red)
+    .cornerRadius(20)
+    .edgesIgnoringSafeArea(.bottom)
+  }
+}
